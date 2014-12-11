38b2db3de2d817b0235e1707f072cc6c0d3d9c93
unknown <areimann@DUS-WKS-B1568.ubisoft.org>
2014-12-11 11:50:52 +0100

    * added basic but untested code for G2::Game class available in GEAR Studio project application code (basic idea of how the code might look is now defined)

--- v2.5/bin/x64/Debug/templates/Game.cpp
+++ v2.5/bin/x64/Debug/templates/Game.cpp
@@ -2,8 +2,55 @@
 
 using namespace G2;
 
+Game* Game::mInstance_ = std::shared_ptr<Game>();
+
+Game&
+Game::getShared()
+{
+	if( mInstance_.get() == nullptr )
+		mInstance_ = std::shared_ptr<Game>(new Game());
+	return *(mInstance_.get());
+}
+
+void Game::destroy()
+{
+	mInstance_ = std::shared_ptr<Game>();
+}
+
+Game::Game() :
+	mCurrentScene("")
+{
+	G2::EventDistributer::onRenderFrame.hook(this, &Game::_onRenderFrame);
+}
+
+Game::~Game()
+{
+	G2::EventDistributer::onRenderFrame.unhookAll(this);
+}
+
 void
 Game::startSceneAsync(std::string const& name) 
 {
-	// generated code goes here
+	// generated code goes here for loading the scene
+	getShared()._scheduleSceneLoading(name);
+}
+
+void
+Game::_scheduleSceneLoading(std::string const& sceneName) 
+{
+	if(sceneName != "")
+	{
+		mCurrentScene = sceneName;
+		mLoadScene = true;
+	}
+}
+
+void
+Game::_onRenderFrame(G2::FrameInfo const&)
+{
+	if(mLoadScene)
+	{
+		// TODO Unload entire ECSManager
+		// TODO load mCurrentScene
+	}
 }
--- v2.5/bin/x64/Debug/templates/Game.h
+++ v2.5/bin/x64/Debug/templates/Game.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Defines.h"
 #include <string>
+#include <memory>
+
+#include <G2/GEAR.h>
 
 namespace G2
 {
@@ -8,10 +11,31 @@ namespace G2
 	{
 		public:
 
+			/** Get a reference to one single instance.
+			 * @return A reference of one single instance.
+			 */
+			static Game& getShared();
+			/** Destroys the one single instance.
+			 */
+			static void destroy();
+
+
 			static void startSceneAsync(std::string const& name);
 
 		private:
+			// don't allow instances
+			Game();
+			Game(Game const&) {}
+			Game& operator=(Game const&) { return *this; }
+			~Game();
 
-			
+			void _scheduleSceneLoading(std::string const& sceneName);
+			void _onRenderFrame(G2::FrameInfo const&);
+
+
+			static std::shared_ptr<Game>	mInstance_;		// The one single instance
+
+			std::string mCurrentScene;
+			bool 		mLoadScene;
 	};
 };
